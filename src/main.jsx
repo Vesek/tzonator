e94f288e4aebc7cc4a09077894e27bcf787d5db4
Vesek <venda.straka@gmail.com>
2025-04-17 13:55:12 +0200

    Fix layout on small devices

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="background w-screen min-h-dvh select-none font-medium text-neutral-800 dark:text-neutral-300">
+    <div className="background relative w-screen min-h-dvh select-none font-medium text-neutral-800 dark:text-neutral-300">
       <div className="flex flex-col items-center justify-center min-h-dvh">
-        <p className="text-5xl min-[320px]:text-7xl font-osifont">TZO-nátor</p>
+        <p className="text-5xl min-[320px]:text-7xl font-osifont mt-4">
+          TZO-nátor
+        </p>
         <App />
       </div>
-      <footer className="absolute bottom-2 right-2">
+      <footer className="absolute bottom-0.5 right-1">
         <p className="text-right text-sm sm:text-base">
           Vytvořil&nbsp;
           <a href="https://github.com/Vesek" className="link">
